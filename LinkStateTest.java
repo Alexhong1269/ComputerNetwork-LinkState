b6bf8ebdefb7ab4d0c4cfed528b1ecf4730ec2e7
Alexhong1269 <Alexhong1269@gmail.com>
2023-11-16 17:51:30 -0500

    trying to use adam's way but link state apprroach

--- LinkStateTest.java
+++ LinkStateTest.java
@@ -2,116 +2,101 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Vector;
-import java.util.HashMap;
-import java.util.InputMismatchException;
-import java.util.Map;
 import java.util.Scanner;
 
-//the Link state program is basically dijkstras algo
-class LinkStateTest{
-    //starting value to find the vertix with min value
+class LinkStateTest {
     static int Vertex = 0;
-    void dijikstra(int graph[][], int src){
-        //distance array to hold the shortest path from source to i
-        int dist[] = new int[Vertex];
 
-        //a set to hold true or false value to see if the vertex is in shortest pathing
+    void dijkstra(int graph[][], int src) {
+        int dist[] = new int[Vertex];
+        int nextHop[] = new int[Vertex]; // Array to store next hop information
+
         Boolean dijSet[] = new Boolean[Vertex];
 
-        //beinging and setting all distance to infinite and pathing value as false
-        for(int i = 0; i < Vertex; i++){
-            //setting the distance to infinte
+        for (int i = 0; i < Vertex; i++) {
             dist[i] = Integer.MAX_VALUE;
-            //setting the pathing to false(not apart of shortest path)
             dijSet[i] = false;
+            nextHop[i] = -1;
         }
 
-        //distance from source to itself is 0
         dist[src] = 0;
 
-        //finding the shortest path(not including itself thats why its -1)
-        for(int i = 1; i < Vertex; i++){
-            //picking the min distance between adjacent vertices from the source
-            int firstPick = minDistance(dist, dijSet);
+        for (int i = 0; i < Vertex - 1; i++) {
+            int firstPick = minDistance(dist, dijSet, nextHop, graph);
 
-            //marking the picked vertex as true(shortest path)
             dijSet[firstPick] = true;
 
-            //updating the adjacent values of the vertices
-            for(int j = 0; j < Vertex; j ++){
-                //check the weighted value of the edges for the shortest path and to check if there is even an edge to a vertex
-                if(!dijSet[j] && graph[firstPick][j] != 0
-                    && dist[firstPick] != Integer.MAX_VALUE
-                    && dist[firstPick] + graph[firstPick][j] < dist[j]){
-                        dist[j] = dist[firstPick] + graph[firstPick][j];
+            for (int j = 0; j < Vertex; j++) {
+                if (!dijSet[j] && graph[firstPick][j] != 0
+                        && dist[firstPick] != Integer.MAX_VALUE
+                        && dist[firstPick] + graph[firstPick][j] < dist[j]) {
+                    dist[j] = dist[firstPick] + graph[firstPick][j];
+                    nextHop[j] = firstPick; // Update next hop information
                 }
             }
-
         }
-        printSol(dist);
+        printSol(dist, nextHop);
     }
 
-    int minDistance(int dist[], Boolean dijSet[]){
-        //setting the min value
-        int min = Integer.MAX_VALUE, min_index = -1;
+    int minDistance(int dist[], Boolean dijSet[], int nextHop[], int graph[][]) {
+        int min = Integer.MAX_VALUE;
+        int min_index = -1;
 
-        for(int i = 0; i < Vertex; i++){
-            //check to see if the pathing is true and if the current distance is less than the min
-            if(dijSet[i] == false && dist[i] <= min){
-                //updating the min distance
+        for (int i = 0; i < Vertex; i++) {
+            if (!dijSet[i] && dist[i] <= min) {
                 min = dist[i];
                 min_index = i;
+            }
+        }
+        
+        if(min_index != -1){
+            for (int i = 0; i < Vertex; i++) {
+                if (i != min_index && graph[min_index][i] != 0 && dist[min_index] + graph[min_index][i] == dist[i]) {
+                    nextHop[i] = min_index;
+                }
             }
         }
         return min_index;
     }
 
-    void printSol(int dist[]){
-        //printing the headers
-        System.out.println("Vertex \t\t Distance from Source");
+    void printSol(int dist[], int nextHop[]) {
+        System.out.println("Vertex \t\t Next Hop \t\t Distance from Source ");
 
-        //printing the traversal number and the distance
-        for(int i = 0; i < Vertex; i++){
-            System.out.println(i + " \t\t " + dist[i]);
+        for (int i = 0; i < Vertex; i++) {
+            System.out.println(i + " \t\t " + nextHop[i] + "\t\t\t\t " + dist[i]);
         }
     }
+
     public static void main(String[] args) throws IOException {
         try (Scanner scanner = new Scanner(new FileReader("topofile.txt"))) {
             int numVertices = scanner.nextInt();
 
             int graph[][] = new int[Vertex][Vertex];
 
-            // // Initialize the graph with zeros
-            // for (int i = 0; i < Vertex; i++) {
-            //     for (int j = 0; j < Vertex; j++) {
-            //         graph[i][j] = 0;
-            //     }
-            // }
-
-            // String line;
             while (scanner.hasNext()) {
                 if (scanner.hasNextInt()) {
-                    int source = scanner.nextInt() - 1; // Adjusting for 0-based indexing
+                    int source = scanner.nextInt() - 1;
                     if (scanner.hasNextInt()) {
                         int destination = scanner.nextInt() - 1;
                         if (scanner.hasNextInt()) {
                             int weight = scanner.nextInt();
-            
-                           //dynamically determing the number of vertices
+
                             int maxVertex = Math.max(source, destination) + 1;
 
-                            Vertex = Math.max(Vertex, maxVertex);
+                            if (maxVertex > Vertex) {
+                                Vertex = maxVertex;
 
-                            graph = new int[Vertex][Vertex];
-            
-                            // Check if indices are within bounds
+                                int newGraph[][] = new int[Vertex][Vertex];
+
+                                for (int i = 0; i < graph.length; i++) {
+                                    System.arraycopy(graph[i], 0, newGraph[i], 0, graph[i].length);
+                                }
+                                graph = newGraph;
+                            }
                             if (source >= 0 && source < Vertex && destination >= 0 && destination < Vertex) {
-                                // Assuming the graph is undirected, update both directions
                                 graph[source][destination] = weight;
                                 graph[destination][source] = weight;
                             } else {
-                                // Print an error message if indices are out of bounds
                                 System.out.println("Error: Indices out of bounds.");
                             }
                         } else {
@@ -121,14 +106,12 @@ class LinkStateTest{
                         System.out.println("Error: Missing destination value.");
                     }
                 } else {
-                    // Consume non-integer tokens or handle the case where there is no next integer
                     scanner.next();
                 }
             }
 
-            // Now, you have the graph ready. Let's find the shortest path.
             LinkStateTest linkStateTest = new LinkStateTest();
-            linkStateTest.dijikstra(graph, 0); // Assuming you want to find the shortest path starting from vertex 0
+            linkStateTest.dijkstra(graph, 0);
         } catch (FileNotFoundException e) {
             System.out.println("File not found");
         } catch (IOException e) {
@@ -136,4 +119,5 @@ class LinkStateTest{
         }
     }
 }
+
 
---
+++ LinkStateTest2.java
@@ -0,0 +1,125 @@
+import java.io.*;
+import java.util.*;
+
+class Graph {
+    class Edge {
+        int dest, weight;
+        Edge(int dest, int weight) {
+            this.dest = dest;
+            this.weight = weight;
+        }
+    }
+
+    int V;
+    public ArrayList<ArrayList<Edge>> adjacencyList;
+
+    public Graph(int v) {
+        V = v;
+        adjacencyList = new ArrayList<>(V);
+        for (int i = 0; i < V; i++) {
+            adjacencyList.add(new ArrayList<>());
+        }
+    }
+
+    void addEdge(int src, int dest, int weight) {
+        adjacencyList.get(src).add(new Edge(dest, weight));
+    }
+
+    void dijkstra(int src, int[] id) {
+        int[] nexthop = new int[V + 1];
+        int[] dist = new int[V + 1];
+        boolean[] dijSet = new boolean[V + 1];
+
+        for (int i = 0; i <= V; i++) {
+            dist[i] = Integer.MAX_VALUE;
+            dijSet[i] = false;
+        }
+
+        dist[src] = 0;
+
+        for (int count = 1; count <= V + 1; count++) {
+            int u = minDistance(dist, dijSet);
+            dijSet[u] = true;
+
+            for (Edge neighbor : adjacencyList.get(u)) {
+                int v = neighbor.dest;
+                int weight = neighbor.weight;
+                if (!dijSet[v] && dist[u] != Integer.MAX_VALUE && dist[u] + weight < dist[v]) {
+                    dist[v] = dist[u] + weight;
+                    nexthop[v] = u;
+                }
+            }
+        }
+        print(dist, id, nexthop);
+    }
+
+    int minDistance(int dist[], boolean dijSet[]) {
+        int min = Integer.MAX_VALUE, min_index = -1;
+
+        for (int i = 1; i <= V; i++) {
+            if (!dijSet[i] && dist[i] <= min) {
+                min = dist[i];
+                min_index = i;
+            }
+        }
+        return min_index;
+    }
+
+    void print(int dist[], int id[], int nexthop[]) {
+        for (int i = 0; i < V + 1; i++) {
+            System.out.println(id[i] + "\t\t\t" + dist[i]);
+        }
+        System.out.println("Output file: ");
+        for (int i = 0; i < dist.length; i++) {
+            System.out.println(id[i] + "\t\t" + (nexthop[id[i]] == 0 ? id[i] : nexthop[id[i]]) + "\t\t" + dist[id[i]]);
+        }
+    }
+
+    public static void main(String[] args) {
+        Set<Integer> Vertices = new HashSet<>();
+        String topo = "topofile.txt";
+        String change = "change.txt";
+
+        try {
+            Scanner scanner = new Scanner(new File(topo));
+
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] parts = line.split("\\s+");
+                if (parts.length == 3) {
+                    int src = Integer.parseInt(parts[0]);
+                    int dest = Integer.parseInt(parts[1]);
+                    Vertices.add(src);
+                    Vertices.add(dest);
+                    int weight = Integer.parseInt(parts[2]);
+                }
+            }
+
+            int numV = Vertices.size();
+            int[] id = Vertices.stream().mapToInt(Integer::intValue).toArray();
+
+            Graph graph = new Graph(numV);
+
+            Scanner scanner2 = new Scanner(new File(topo));
+
+            while (scanner2.hasNextLine()) {
+                String line = scanner2.nextLine();
+                String[] parts = line.split("\\s+");
+                if (parts.length == 3) {
+                    int src = Integer.parseInt(parts[0]);
+                    int dest = Integer.parseInt(parts[1]);
+                    int weight = Integer.parseInt(parts[2]);
+                    graph.addEdge(src, dest, weight);
+                }
+            }
+
+            for (int i = 1; i <= numV; i++) {
+                System.out.println("Source Node: " + i + "   Distance from Source");
+                graph.dijkstra(i, id);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.err.println("File not found. Make sure the file path is correct.");
+        }
+    }
+}
